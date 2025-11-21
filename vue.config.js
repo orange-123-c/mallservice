b9568ec5d3731a7b6e1055e78f677f43b10476e2
cyz <2162006819@qq.com>
2025-11-21 19:24:12 +0800

    修复视频背景问题，更新代码

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  // 修改为相对路径，支持本地文件打开
+  // 保留相对路径 ./
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   
-  // 以下是你原有配置
   transpileDependencies: true,
   lintOnSave: false // 关闭语法检查
 })
